ff8ca47b9818ba4a8b8860572c418c122e94e2d3
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-11 01:28:52 +0900

    added helpe file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Game from './components/Game'
+import { calculateWinner, squares } from './helpers';
 
 const App = () => {
   return (
     <div>
       <Game />
+      {console.log(calculateWinner(squares))}
     </div>
   )
 }
