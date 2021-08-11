f47c8c6857747805acb61ebb0ed4fe6c74a1b205
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-11 12:34:07 +0900

    added logic for winner and next player

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,15 +5,7 @@ const Board = (props) => {
   const { squares, onClick } = props;
   return (
     <div className="board">
-      <Square value="1" onClick={onClick} />
-      <Square value="2" onClick={onClick} />
-      <Square value="3" onClick={onClick} />
-      <Square value="4" onClick={onClick} />
-      <Square value="5" onClick={onClick} />
-      <Square value="6" onClick={onClick} />
-      <Square value="7" onClick={onClick} />
-      <Square value="8" onClick={onClick} />
-      <Square value="9" onClick={onClick} />
+      {squares.map((square, i) => <Square key={i} value={square} onClick={() => onClick(i)} /> )}
     </div>
   )
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 import calculateWinner from '../helpers';
 
 const Game = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(board);
 
-  const handleClick = () => {
-    console.log('Clicked');
+  const handleClick = (i) => {
+    const boardCopy = [...board];
+    if (winner || boardCopy[i]) return;
+    boardCopy[i] = xIsNext ? 'X' : 'O';
+    setBoard(boardCopy);
+    setXisNext(!xIsNext);
   }
 
   return (
-    <Board onClick={handleClick}/>
+    <>
+      <Board squares={board} onClick={handleClick}/>
+      <div className='results'>
+        <p>{winner ? `${winner} is the winner!`: `Next player: ${xIsNext ? 'X' : 'O'}` }</p>
+      </div>
+    </>
   )
 }
 
