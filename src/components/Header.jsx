27dfcccf2ce1b5be422f313afcd21ec3f297ca93
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-11 14:30:14 +0900

    added game logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGamepad } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   return (
     <div className="header">
+      <FontAwesomeIcon icon={faGamepad} className="logo" />
       <h1>React Tic-tac-toe</h1>
     </div>
   )
